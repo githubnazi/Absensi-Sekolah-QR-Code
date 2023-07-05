90bccc30977c7cae36c71f4d7d791185e81bcccc
githubnazi <129722620+githubnazi@users.noreply.github.com>
2023-07-05 11:16:57 +0700

    Update scan.php
    1. Penambahan RFID input field
    2. Mengganti alert menjadi label karena penambahan auto reload page dan focus ke RFID field.

--- app/Views/scan/scan.php
+++ app/Views/scan/scan.php
@@ -1,203 +1,241 @@
- <?= $this->extend('templates/starting_page_layout'); ?>
+<?= $this->extend('templates/starting_page_layout'); ?>
 
- <?= $this->section('navaction') ?>
- <a href="<?= base_url('/admin'); ?> " class="btn btn-primary pull-right pl-3">
+<?= $this->section('navaction') ?>
+<a href="<?= base_url('/admin'); ?> " class="btn btn-primary pull-right pl-3">
     <i class="material-icons mr-2">dashboard</i>
     Dashboard Petugas
- </a>
- <?= $this->endSection() ?>
+</a>
+<?= $this->endSection() ?>
 
- <?= $this->section('content'); ?>
- <?php
-   $oppBtn = '';
+<?= $this->section('content'); ?>
+<?php
+    $oppBtn = '';
 
-   $waktu == 'Masuk' ? $oppBtn = 'pulang' : $oppBtn = 'masuk';
-   ?>
- <div class="main-panel">
+    $waktu == 'Masuk' ? $oppBtn = 'pulang' : $oppBtn = 'masuk';
+?>
+<div class="main-panel">
     <div class="content">
-       <div class="container-fluid">
-          <div class="row">
-             <div class="mx-auto" style="max-width: 25vw;">
-                <div class="card">
-                   <div class="card-body">
-                      <h3 class="mt-2"><b>Tips</b></h3>
-                      <ul class="pl-3">
-                         <li>Tunjukkan qr code sampai terlihat jelas di kamera</li>
-                         <li>Posisikan qr code tidak terlalu jauh maupun terlalu dekat</li>
-                      </ul>
-                   </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="mx-auto" style="max-width: 25vw;">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="mt-2"><b>Tips</b></h3>
+                            <ul class="pl-3">
+                                <li>Tunjukkan qr code sampai terlihat jelas di kamera</li>
+                                <li>Posisikan qr code tidak terlalu jauh maupun terlalu dekat</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="errorMessage" class="alert alert-danger" style="display: none;"></div>
                 </div>
-             </div>
-             <div class="mx-auto">
-                <div class="card">
-                   <div class="col-sm-10 mx-auto card-header card-header-primary">
-                      <div class="row">
-                         <div class="col">
-                            <h4 class="card-title"><b>Absen <?= $waktu; ?></b></h4>
-                            <p class="card-category">Silahkan tunjukkan QR Code anda</p>
-                         </div>
-                         <div class="col-md-auto">
-                            <a href="<?= base_url("scan/$oppBtn"); ?>" class="btn btn-<?= $oppBtn == 'masuk' ? 'success' : 'warning'; ?>">
-                               Absen <?= $oppBtn; ?>
-                            </a>
-                         </div>
-                      </div>
-                   </div>
-                   <div class="card-body my-auto px-5">
-                      <!-- <h4 class="d-inline">Pilih kamera</h4>
-
-                      <select id="pilihKamera" class="custom-select w-50 ml-2" aria-label="Default select example" style="height: 35px;">
-                         <option selected>Select camera devices</option>
-                      </select> -->
-
-                      <br>
-
-                      <div class="row">
-                         <div class="col-sm-12 mx-auto">
-                            <div class="previewParent">
-                               <div class="text-center">
-                                  <h4 class="d-none w-100" id="searching"><b>Mencari...</b></h4>
-                               </div>
-                               <video id="previewKamera"></video>
+                <div class="mx-auto">
+                    <div class="card">
+                        <div class="col-sm-10 mx-auto card-header card-header-primary">
+                            <div class="row">
+                                <div class="col">
+                                    <h4 class="card-title"><b>Absen <?= $waktu; ?></b></h4>
+                                    <p class="card-category">Silahkan tunjukkan QR Code anda</p>
+                                </div>
+                                <div class="col-md-auto">
+                                    <a href="<?= base_url("scan/$oppBtn"); ?>"
+                                        class="btn btn-<?= $oppBtn == 'masuk' ? 'success' : 'warning'; ?>">
+                                        Absen <?= $oppBtn; ?>
+                                    </a>
+                                </div>
                             </div>
-                         </div>
-                      </div>
-                      <div id="hasilScan"></div>
-                      <br>
-                   </div>
-                </div>
-             </div>
-             <div class="mx-auto" style="max-width: 25vw;">
-                <div class="card">
-                   <div class="card-body">
-                      <h3 class="mt-2"><b>Penggunaan</b></h3>
-                      <ul class="pl-3">
-                         <li>Jika berhasil scan maka akan muncul data siswa/guru dibawah preview kamera</li>
-                         <li>Klik tombol <b><span class="text-success">Absen masuk</span> / <span class="text-warning">Absen pulang</span></b> untuk mengubah waktu absensi</li>
-                         <li>Untuk melihat data absensi, klik tombol <span class="text-primary"><i class="material-icons" style="font-size: 16px;">dashboard</i> Dashboard Petugas</span></li>
-                         <li>Untuk mengakses halaman petugas anda harus login terlebih dahulu</li>
-                      </ul>
-                   </div>
-                </div>
-             </div>
-          </div>
-       </div>
-    </div>
- </div>
+                        </div>
+                        <div class="card-body my-auto px-5">
+                            <!-- <h4 class="d-inline">Pilih kamera</h4>
 
- <script type="text/javascript" src="<?= base_url('public/assets/js/plugins/zxing/zxing.min.js') ?>"></script>
- <script src="<?= base_url('public/assets/js/plugins/jquery/jquery-3.5.1.min.js') ?>"></script>
- <script type="text/javascript">
+                            <select id="pilihKamera" class="custom-select w-50 ml-2" aria-label="Default select example"
+                                style="height: 35px;">
+                                <option selected>Select camera devices</option>
+                            </select> -->
+
+                            <br>
+
+                            <div class="row">
+                                <div class="col-sm-12 mx-auto">
+                                    <div class="previewParent">
+                                        <div class="text-center">
+                                            <h4 class="d-none w-100" id="searching"><b>Mencari...</b></h4>
+                                        </div>
+                                        <video id="previewKamera"></video>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="hasilScan"></div>
+                            <br>
+                            <form id="scanForm">
+                                <div class="form-group">
+                                    <label for="uniqueCodeInput">Unique Code:</label>
+                                    <input type="text" class="form-control" id="uniqueCodeInput" name="unique_code"
+                                        required>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="mx-auto" style="max-width: 25vw;">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="mt-2"><b>Penggunaan</b></h3>
+                            <ul class="pl-3">
+                                <li>Jika berhasil scan maka akan muncul data siswa/guru dibawah preview
+                                    kamera</li>
+                                <li>Klik tombol <b><span class="text-success">Absen masuk</span> / <span
+                                            class="text-warning">Absen pulang</span></b> untuk mengubah waktu
+                                    absensi</li>
+                                <li>Untuk melihat data absensi, klik tombol <span class="text-primary"><i
+                                            class="material-icons" style="font-size: 16px;">dashboard</i> Dashboard
+                                        Petugas</span></li>
+                                <li>Untuk mengakses halaman petugas anda harus login terlebih dahulu</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="<?= base_url('public/assets/js/plugins/zxing/zxing.min.js') ?>"></script>
+<script src="<?= base_url('public/assets/js/plugins/jquery/jquery-3.5.1.min.js') ?>"></script>
+<script type="text/javascript">
     let selectedDeviceId = null;
     let audio = new Audio("<?= base_url('public/assets/audio/beep.mp3'); ?>");
     const codeReader = new ZXing.BrowserMultiFormatReader();
     const sourceSelect = $('#pilihKamera');
 
-    $(document).on('change', '#pilihKamera', function() {
-       selectedDeviceId = $(this).val();
-       if (codeReader) {
-          codeReader.reset();
-          initScanner();
-       }
+    $(document).on('change', '#pilihKamera', function () {
+        selectedDeviceId = $(this).val();
+        if (codeReader) {
+            codeReader.reset();
+            initScanner();
+        }
     })
 
     const previewParent = document.getElementById('previewParent');
     const preview = document.getElementById('previewKamera');
 
     function initScanner() {
-       codeReader.listVideoInputDevices()
-          .then(videoInputDevices => {
-             videoInputDevices.forEach(device =>
-                console.log(`${device.label}, ${device.deviceId}`)
-             );
+        codeReader.listVideoInputDevices()
+            .then(videoInputDevices => {
+                videoInputDevices.forEach(device =>
+                    console.log(`${device.label}, ${device.deviceId}`)
+                );
 
-             if (videoInputDevices.length < 1) {
-                alert("Camera not found!");
-                return;
-             }
-
-             if (selectedDeviceId == null) {
-                if (videoInputDevices.length <= 1) {
-                   selectedDeviceId = videoInputDevices[0].deviceId
-                } else {
-                   selectedDeviceId = videoInputDevices[1].deviceId
+                if (videoInputDevices.length < 1) {
+                    // alert("Camera not found!");
+                     $('#errorMessage').text("Camera not found!").show();
+                    return;
                 }
-             }
 
-             if (videoInputDevices.length >= 1) {
-                sourceSelect.html('');
-                videoInputDevices.forEach((element) => {
-                   const sourceOption = document.createElement('option')
-                   sourceOption.text = element.label
-                   sourceOption.value = element.deviceId
-                   if (element.deviceId == selectedDeviceId) {
-                      sourceOption.selected = 'selected';
-                   }
-                   sourceSelect.append(sourceOption)
-                })
-             }
+                if (selectedDeviceId == null) {
+                    if (videoInputDevices.length <= 1) {
+                        selectedDeviceId = videoInputDevices[0].deviceId
+                    } else {
+                        selectedDeviceId = videoInputDevices[1].deviceId
+                    }
+                }
 
-             $('#previewParent').removeClass('unpreview');
-             $('#previewKamera').removeClass('d-none');
-             $('#searching').addClass('d-none');
+                if (videoInputDevices.length >= 1) {
+                    sourceSelect.html('');
+                    videoInputDevices.forEach((element) => {
+                        const sourceOption = document.createElement('option')
+                        sourceOption.text = element.label
+                        sourceOption.value = element.deviceId
+                        if (element.deviceId == selectedDeviceId) {
+                            sourceOption.selected = 'selected';
+                        }
+                        sourceSelect.append(sourceOption)
+                    })
+                }
 
-             codeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'previewKamera')
-                .then(result => {
-                   console.log(result.text);
-                   cekData(result.text);
+                $('#previewParent').removeClass('unpreview');
+                $('#previewKamera').removeClass('d-none');
+                $('#searching').addClass('d-none');
 
-                   $('#previewKamera').addClass('d-none');
-                   $('#previewParent').addClass('unpreview');
-                   $('#searching').removeClass('d-none');
+                codeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'previewKamera')
+                    .then(result => {
+                        console.log(result.text);
+                        cekData(result.text);
 
-                   if (codeReader) {
-                      codeReader.reset();
+                        $('#previewKamera').addClass('d-none');
+                        $('#previewParent').addClass('unpreview');
+                        $('#searching').removeClass('d-none');
 
-                      // delay 2,5 detik setelah berhasil meng-scan
-                      setTimeout(() => {
-                         initScanner();
-                      }, 2500);
-                   }
-                })
-                .catch(err => console.error(err));
+                        if (codeReader) {
+                            codeReader.reset();
 
-          })
-          .catch(err => console.error(err));
+                            // delay 2,5 detik setelah berhasil meng-scan
+                            setTimeout(() => {
+                                initScanner();
+                            }, 2500);
+                        }
+                    })
+                    .catch(err => console.error(err));
+
+            })
+            .catch(err => console.error(err));
     }
 
     if (navigator.mediaDevices) {
-       initScanner();
+        initScanner();
     } else {
-       alert('Cannot access camera.');
+        // alert('Cannot access camera.');
+      $('#errorMessage').text("Cannot access camera.").show();
     }
 
     async function cekData(code) {
-       jQuery.ajax({
-          url: "<?= base_url('scan/cek'); ?>",
-          type: 'post',
-          data: {
-             'unique_code': code,
-             'waktu': '<?= strtolower($waktu); ?>'
-          },
-          success: function(response, status, xhr) {
-             audio.play();
-             console.log(response);
-             $('#hasilScan').html(response);
+        jQuery.ajax({
+            url: "<?= base_url('scan/cek'); ?>",
+            type: 'post',
+            data: {
+                'unique_code': code,
+                'waktu': '<?= strtolower($waktu); ?>'
+            },
+            success: function (response, status, xhr) {
+                audio.play();
+                console.log(response);
+                $('#hasilScan').html(response);
 
-             $('html, body').animate({
-                scrollTop: $("#hasilScan").offset().top
-             }, 500);
-          },
-          error: function(xhr, status, thrown) {
-             console.log(thrown);
-             $('#hasilScan').html(thrown);
-          }
-       });
+                $('html, body').animate({
+                    scrollTop: $("#hasilScan").offset().top
+                }, 500);
+            },
+            error: function (xhr, status, thrown) {
+                console.log(thrown);
+                $('#hasilScan').html(thrown);
+            }
+        });
     }
 
     function clearData() {
-       $('#hasilScan').html('');
+        $('#hasilScan').html('');
     }
- </script>
 
- <?= $this->endSection(); ?>
+    // Handle form submission
+    $('#scanForm').submit(function (e) {
+        e.preventDefault();
+        const uniqueCode = $('#uniqueCodeInput').val();
+        if (uniqueCode) {
+            cekData(uniqueCode);
+            $('#uniqueCodeInput').val('');
+        }
+    });
+
+        // Refresh page every 5 seconds
+    setInterval(function () {
+        location.reload();
+    }, 10000);
+
+    // Put cursor in unique code form input
+    $(document).ready(function () {
+        $('#uniqueCodeInput').focus();
+    });
+</script>
+
+<?= $this->endSection(); ?>
